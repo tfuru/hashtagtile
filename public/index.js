fb549fdf622835daeae765049f545e0c7b7e7577
t_furu <t_furu@tf-web.jp>
2018-11-12 12:53:42 +0900

    update css

--- public/index.js
+++ public/index.js
@@ -4,51 +4,50 @@ class App {
         console.log("constructor");
         try {
             this.firebaseApp = firebase.app();
-            //let features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof this.firebaseApp[feature] === 'function');
             this.database = this.firebaseApp.database();
         } catch (e) {
             console.error(e);
         }
-
-        this.vueApp = new Vue({
-            el: '#columns',
-            data: {
-                tweets:[]
-            }
-        });
     };
 
     onLoaded(){
+        this.vueApp = new Vue({
+            el: '#app',
+            data: {
+                hashtag:"",
+                tweets:[],
+                isModalInfo: false,
+                bosySample:'{"UserName":"<<<{{UserName}}>>>","CreatedAt":"{{CreatedAt}}","UserImageUrl":"{{UserImageUrl}}","FirstLinkUrl":"{{FirstLinkUrl}}","LinkToTweet":"{{LinkToTweet}}"}'
+            },
+            methods: {
+                openItem() {
+                    console.log("openItem");
+                    this.toggleModal();
+                },
+                toggleModal() {
+                    this.isModalInfo = !this.isModalInfo;
+                }
+            }
+        });
+
         //URLにつけたハッシュ・タグを取得する
-        let hashtag = location.hash.replace("#","").trim();
-        console.log("onLoaded",hashtag);
+        this.vueApp.hashtag = location.hash.replace("#","").trim();
+        console.log("onLoaded",this.vueApp.hashtag);
 
         //DB更新 イベントの読み取り
         let fnc =  this.onChildAdded.bind(this);
-        this.database.ref('/tweets/'+hashtag).on('child_added', fnc);
+        this.database.ref('/tweets/'+this.vueApp.hashtag).limitToLast(100).on('child_added', fnc);
     };
 
     onChildAdded(snapshot){
         console.log("onChildAdded",snapshot.key,snapshot.val());
-        //console.log("this.vueApp",this.vueApp);
         var tweet = snapshot.val();
-        //TODO RTの場合処理しない
-        
+
         if(typeof(tweet.ImageUrl) == "undefined"){
-            tweet.ImageUrl = "150x150.png";
+            tweet.ImageUrl = "img/150x150.png";
         }
 
-        //IFTTTのエスケープを外す
-        tweet.UserName = tweet.UserName.replace("<<","").replace(">>","");
-        tweet.Text = tweet.Text.replace("<<","").replace(">>","");
-
         this.vueApp.tweets.push(tweet);
-        /*
-        CreatedAt
-        Text
-        UserImageUrl
-        UserName
-        */
     };
 };
 
